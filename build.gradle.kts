a6c847adcfd2d60b9bc38458b8463d5fe6e5bf1c
Arvik <94380788+arviik@users.noreply.github.com>
2024-12-02 11:50:58 +0100

    feat(api): Added .env file support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 val exposed_version: String by project
-val h2_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val dotenv_kotlin_version: String by project
+val postgresql_driver_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.21"
@@ -30,11 +31,14 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposed_version")
+    implementation("org.postgresql:postgresql:$postgresql_driver_version")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml-jvm")
+    implementation("io.github.cdimascio:dotenv-kotlin:$dotenv_kotlin_version")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
